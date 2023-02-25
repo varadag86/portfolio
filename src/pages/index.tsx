7e2917d19abf142c8c8e0f8bd7db5dfe1d9de8bc
Varad <py.varad@gmail.com>
2023-02-25 21:42:50 +0530

    Updated image position

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,7 @@ const IndexPage: React.FC<PageProps> = () => {
           <div className="container">
             <div className="is-flex is-vcentered">
               <div className="columns">
-                <div className="column">
+                <div className="column is-8">
                   <div className="user__info has-text-left has-text-justified">
                     <div className="mt-4">
                       <p>Hello ! My name is <span data-tooltip="Tooltip Text">Varad</span>, and I'm a Bangalore based software developer. While I am not working I spend time on travelling, sketching and photography.</p>
@@ -25,16 +25,16 @@ const IndexPage: React.FC<PageProps> = () => {
                       <p>I am a fullstack developer with diverse skill set from HTML, CSS and Javascript on frontend. In the backend development I use Java with Springboot or Python with Flask.</p>
                     </div>
 
-                    <div className="pt-4 mt-4">
+                    <div className="pt-4 mt-4 is-flex is-justify-content-center">
                       <span className="mt-4 p-4"><img src={LINKEDIN} height={20} width={20} /></span>
                       <span className="mt-4 p-4"><img src={STACK} height={20} width={20} /></span>
                       <span className="mt-4 p-4"><img src={INSTA} height={20} width={20} /></span>
                     </div>
                   </div>
                 </div>
-                <div className="column">
-                  <div className="image is-128x128">
-                    <img src={PORTFOLIO_IMG} className='is-rounded is-pulled-right' />
+                <div className="column is-4">
+                  <div className="image">
+                    <img src={PORTFOLIO_IMG} className='is-rounded is-pulled-right' style={{ height: '320px', width: '320px' }} />
                   </div>
                 </div>
               </div>
