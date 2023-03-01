1e708e8640143ec8c8898d04314a3f89100e26e9
Varad <py.varad@gmail.com>
2023-03-01 10:06:59 +0530

    Updated the blogs page with new gradle reporsitory

--- src/pages/blogs.tsx
+++ src/pages/blogs.tsx
@@ -23,10 +23,27 @@ const Blogs = () => {
                             <p className="m-4 is-size-6">The springboot base gradle project is a simple gradle project with boilerplates included. This code base involves standard code elements while building a springboot microserivices and best practices suggested. It will not include database configurations or a integration code such as connectors for kafka or with cloud services.</p>
                             <div className="columns">
                                 <div className="column">
-                                    <p className="m-4"><strong>Read Me:</strong> <a href="https://varadag86.github.io/spring-base-gradle/" target="_blank">Springboot Gradle Project</a></p>
+                                    <p className="m-4"><strong>Read Me:</strong> <a href="https://varadag86.github.io/spring-base-gradle/" target="_blank">Docs</a></p>
                                 </div>
                                 <div className="column is-flex is-justify-content-end">
-                                    <p className="m-4"><strong>Repository:</strong> <a href="https://github.com/varadag86/spring-base-gradle" target="_blank">Springboot Repository</a></p>
+                                    <p className="m-4"><strong>Repository:</strong> <a href="https://github.com/varadag86/spring-base-gradle" target="_blank">Repository</a></p>
+                                </div>
+                            </div>
+                        </li>
+                        <li style={{
+                            background: '#FAFAFA',
+                            margin: '8px',
+                            padding: '12px',
+                            borderRadius: '8px'
+                        }}>
+                            <p className="mb-4 is-size-5">Springboot Postgres App</p>
+                            <p className="m-4 is-size-6">The springboot postgres app focus on setting up the base template for postgres with the dependencies needed to connect to the database. Apart from that it will have standard practies while build a postgres app with spring-data-jpa and reference test cases.</p>
+                            <div className="columns">
+                                <div className="column">
+                                    <p className="m-4"><strong>Read Me:</strong> <a href="https://varadag86.github.io/spring-templates/" target="_blank">Docs.</a></p>
+                                </div>
+                                <div className="column is-flex is-justify-content-end">
+                                    <p className="m-4"><strong>Repository:</strong> <a href="https://github.com/varadag86/spring-templates/tree/develop/springboot-gradle-postgres" target="_blank">Repository</a></p>
                                 </div>
                             </div>
                         </li>
